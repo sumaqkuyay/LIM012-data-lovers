eaff7cfb49bbc3a64d5ad844662e0c201d57b4a0
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-04-06 05:14:37 -0500

    FILTRADO Y ORDENADO DE POKEMON

--- src/main.js
+++ src/main.js
@@ -1,55 +1,76 @@
 // import data from './data/atletas/atletas.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-import { calStab, caldps, caleps } from './data.js';
+import { calStab, caldps, caleps, sortByType, sortMaxCp } from './data.js';
+
+const pokemonListing = data.pokemon;
 
 
-let output = '';
+const toShow = ((pokeList) => {
+  let output = '';
+  pokeList.forEach((pokemon) => {
+    let type = '';
+    pokemon.type.forEach((tipo) => {
+      type += `<div class="${tipo}">${tipo}</div>`;
+    });
 
-data.pokemon.forEach((pokemon) => {
+    let dataQMove='';
+    pokemon['quick-move'].forEach((ataque) => {
+      dataQMove += `"${ataque.name}"tipo:"${ataque.type}" base:"${ataque['base-damage']}"  `;
+      const found = pokemon.type.find(element => element === ataque.type);
 
-  let type = '';
-  pokemon.type.forEach((tipo) => {
-    type += `<div>${tipo}</div>`;
-  });
+      const stabQM = calStab(found, ataque.type, ataque['base-damage']);
+      const dpsQM = caldps(ataque['base-damage'], stabQM, ataque['move-duration-seg']);
+      const epsQM = caleps(ataque.energy, ataque['move-duration-seg']);
+    });
 
-  let dataQMove='';
-  pokemon['quick-move'].forEach((ataque) => {
-    dataQMove += `"${ataque.name}"tipo:"${ataque.type}" base:"${ataque['base-damage']}"  `;
-    const found = pokemon.type.find(element => element === ataque.type);
+    let dataEAttack='';
+    pokemon['special-attack'].forEach((ataque) => {
+      const found = pokemon.type.find(element => element === ataque.type);
 
-    const stabQM = calStab(found, ataque.type, ataque['base-damage']);
-    const dpsQM = caldps(ataque['base-damage'], stabQM, ataque['move-duration-seg']);
-    const epsQM = caleps(ataque.energy, ataque['move-duration-seg']);
-  });
+      const stabEA = calStab(found, ataque.type, ataque['base-damage']);
+      const dpsEA = caldps(ataque['base-damage'], stabEA, ataque['move-duration-seg']);
+      const epsEA = caleps(ataque.energy, ataque['move-duration-seg']);
+      dataEAttack += `"${ataque.name}"tipo:"${ataque.type}" base:"${ataque['base-damage']}" ${stabEA} ${dpsEA} ${epsEA} `;
+  
+    });
 
-  let dataEAttack='';
-  pokemon['special-attack'].forEach((ataque) => {
-    dataEAttack += `"${ataque.name}"tipo:"${ataque.type}" base:"${ataque['base-damage']}"  `;
-    const found = pokemon.type.find(element => element === ataque.type);
-
-    const stabEA = calStab(found, ataque.type, ataque['base-damage']);
-    const dpsEA = caldps(ataque['base-damage'], stabEA, ataque['move-duration-seg']);
-    const epsEA = caleps(ataque.energy, ataque['move-duration-seg']);
-  });
-
-  const dataPoke = `
-  <div class="dataGeneral" >
-    <div class="card">
-        <div class="frontSide">
-            <div id="pokemonCard" class="pokemonCard">
-                <div id="number" class="number">${pokemon.num}</div>
-                <img src=${pokemon.img} class="imgPokemon">
-                <p id="pokeName" class="pokeName">${pokemon.name.slice(0, 1).toUpperCase()}${pokemon.name.slice(1)}</p>
-                <div class="maxCp"> Max - cp : ${pokemon.stats['max-cp']} </div>
-                <div class="types"> ${type}</div>
-            </div>
-            <div class="backSide"></div>
-        </div>
+    const dataPoke = `
+    <div class="dataGeneral" >
+      <div class="card">
+          <div class="frontSide">
+              <div id="pokemonCard" class="pokemonCard">
+                  <div id="number" class="number">${pokemon.num}</div>
+                  <img src=${pokemon.img} class="imgPokemon">
+                  <p id="pokeName" class="pokeName">${pokemon.name.slice(0, 1).toUpperCase()}${pokemon.name.slice(1)}</p>
+                  <div class="maxCp"> Max - cp : ${pokemon.stats['max-cp']} </div>
+                  <div class="types"> ${type}</div>
+              </div>
+          </div>
+          <div class="backSide" hidden>
+         ${dataEAttack}
+          </div>
+      </div>
     </div>
-  </div>
-  `;
-  output += dataPoke;
+    `;
+    output += dataPoke;
+  });
+  document.getElementById('dataGHTML').innerHTML = output;
 });
 
-document.getElementById('dataGHTML').innerHTML = output;
+toShow(pokemonListing);
+
+const listType = document.querySelectorAll('#typePoke > ul > .menutype');
+
+listType.forEach((item) => {
+  item.addEventListener('click', () => {
+    toShow(sortByType(pokemonListing, item.innerHTML));
+  });
+});
+
+const listMaxCp = document.querySelectorAll('#maxcp > ul > .orderbtn');
+listMaxCp.forEach((item) => {
+  item.addEventListener('click', () => {
+    toShow(sortMaxCp(pokemonListing, item.innerHTML));
+  });
+});
